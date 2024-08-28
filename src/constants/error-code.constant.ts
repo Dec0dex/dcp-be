c94837b47ee6a14346e3612340fa02af94d372ec
Dejan Radmanovic <office@decodex.net>
2024-08-28 06:15:43 +0200

    feat(dcp-210): setup backend mvp

---
+++ src/constants/error-code.constant.ts
@@ -0,0 +1,12 @@
+export enum ErrorCode {
+  // Common Validation
+  V0000 = 'common.validation.error',
+
+  // Validation
+  V0001 = 'user.validation.is_empty',
+  V0002 = 'user.validation.is_invalid',
+
+  // Error
+  E0001 = 'user.error.username_or_email_exists',
+  E0002 = 'user.error.not_found',
+}
